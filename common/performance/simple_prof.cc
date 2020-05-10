8e403848e826d4bb0407472a03a4c0ad5815f368
edward.liu <liuyongchuan@outlook.com>
2020-05-10 18:53:13 +0800

    add kitti support and a simple profiling tool

---
+++ common/performance/simple_prof.cc
@@ -0,0 +1,95 @@
+// MIT License
+
+// Copyright (c) 2019 Edward Liu
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "common/performance/simple_prof.h"
+
+namespace static_map {
+namespace common {
+namespace performance {
+
+struct StatisticDetails {
+  size_t size;
+  int64_t average;
+  int64_t max;
+  int64_t min;
+  int64_t sum;
+};
+
+inline StatisticDetails GetStatistics(std::vector<int64_t>& data) {
+  std::sort(data.begin(), data.end());
+  StatisticDetails statistics;
+  statistics.size = data.size();
+  statistics.max = data.back();
+  statistics.min = data.front();
+
+  statistics.sum = 0;
+  for (const auto& d : data) {
+    statistics.sum += d;
+  }
+  statistics.average = statistics.sum / statistics.size;
+  return statistics;
+}
+
+std::vector<int64_t>* TimerManager::RegisterTimer(
+    const std::string& block_name) {
+  std::lock_guard<std::mutex> locker(mutex_);
+  return &durations_[std::this_thread::get_id()][block_name];
+}
+
+TimerManager::~TimerManager() {
+  std::cout << std::endl;
+  std::cout << "\e[1;31m" << std::setw(25) << "block name"
+            << " | " << std::setw(8) << "times"
+            << " | " << std::setw(9) << "avg.(us)"
+            << " | " << std::setw(10) << "sum.(us)"
+            << " | " << std::setw(9) << "min.(us)"
+            << " | " << std::setw(9) << "max.(us)"
+            << "\e[0m" << std::endl;
+  for (int i = 0; i < 85; ++i) {
+    std::cout << "-";
+  }
+  std::cout << std::endl;
+
+  const auto time_to_string = [](const int64_t time) -> std::string {
+    return (time >= 1 ? std::to_string(time) : "<1");
+  };
+
+  for (auto& id_block : durations_) {
+    for (auto& name_vector : id_block.second) {
+      const auto statistics = GetStatistics(name_vector.second);
+
+      std::cout << std::setw(25) << name_vector.first << " | " << std::setw(8)
+                << statistics.size << " | " << std::setw(9)
+                << time_to_string(statistics.average) << " | " << std::setw(10)
+                << time_to_string(statistics.sum) << " | " << std::setw(9)
+                << time_to_string(statistics.min) << " | " << std::setw(9)
+                << time_to_string(statistics.max) << std::endl;
+    }
+  }
+  std::cout << std::endl;
+}
+
+}  // namespace performance
+}  // namespace common
+}  // namespace static_map
+
+INIT_PROF;
---
+++ mapping_kitti.sh
@@ -0,0 +1,19 @@
+## usally, you can just leave this config file just like this, it will work fine
+CONFIG_PATH=./config/lidar_only.xml
+URDF_FILE=./urdf/test2.urdf
+## the follow 2 items must be set!!!
+## the topic name of your pointcloud msg (ros)
+POINT_CLOUD_TOPIC=velodyne_points
+## the frame id of your pointcloud msg (ros)
+POINT_CLOUD_FRAME_ID=frame_velodyne_points
+
+KITTI_PATH=/mnt/Data/kitti/lidar/00/velodyne
+
+./build/static_mapping_node \
+  -cfg ${CONFIG_PATH} \
+  -pc ${POINT_CLOUD_TOPIC} \
+  -pc_frame_id ${POINT_CLOUD_FRAME_ID} \
+  -urdf ${URDF_FILE} \
+  -kitti ${KITTI_PATH}
+
+exit 0 
